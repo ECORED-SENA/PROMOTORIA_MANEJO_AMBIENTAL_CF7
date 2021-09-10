02dc72b393cc9df322b4a9b79338af1ba96113e4
luisEfe80 <luismendez80@hotmail.com>
2021-09-10 07:54:50 -0400

    Ajuste de transicion

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,7 +6,7 @@
       .titulo-principal__numero
         span 3
       h1 Responsabilidad social
-    figure.mb-5(data-aos="fade-up")
+    figure.mb-5
       .video
         iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
