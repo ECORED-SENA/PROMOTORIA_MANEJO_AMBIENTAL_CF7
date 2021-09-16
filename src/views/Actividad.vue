327aad231891ccf5ff14cf3f1b377993a21101e7
luisEfe80 <luismendez80@hotmail.com>
2021-09-16 14:40:50 -0400

    Adicion de actividad didactica

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -16,7 +16,7 @@
           .row.justify-content-left.align-items-left      
 
             .col-auto
-              a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
+              a.boton.boton--b(:href="obtenerLink('/actividades/story.html')" target="_blank")
                 span Realizar
                 i.fas.fa-puzzle-piece
 
